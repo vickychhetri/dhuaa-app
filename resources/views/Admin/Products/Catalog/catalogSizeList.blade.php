7c403213a842cbba4fb2bb02b0958f9081230d78
vicky chhetri <specialpower321@gmail.com>
2021-12-30 15:00:07 +0530

    padding added to move :2

--- resources/views/Admin/Products/Catalog/catalogSizeList.blade.php
+++ resources/views/Admin/Products/Catalog/catalogSizeList.blade.php
@@ -43,10 +43,10 @@
                             @foreach($notListedProducts as $product)
                             <tr>
                                 <td>
-                                    <div style="border: 1px solid black;background-color:#eaf0e6;padding:16px;">
+                                    <span style="border: 1px solid black;background-color:#eaf0e6;padding:16px;">
                                         <span style="float: left;"> {{$product->productName}} </span>
                                         <a href="#" class="btn btn-default" style="float: right;"> Move</a>
-                                    </div>
+                                    </span>
                                 </td>
                             </tr>
                             @endforeach
